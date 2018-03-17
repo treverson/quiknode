f48c672714918cbf4218f8d909b3aa3489cdc5f7
webosmotic-developer <dev.webosmotic@gmail.com>
2018-03-17 12:54:40 +0530

    Setup proxy server for aws.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var httpProxy = require('http-proxy');
+const https = require('https')
+var apiProxy = httpProxy.createProxyServer();
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'dist')));
+app.all("/api/*", function(req, res){
+    apiProxy.web(req, res, { target: 'https://quiknode.pro', agent: https.globalAgent,
+        headers: {
+            host: 'quiknode.pro'
+        } });
+});
 app.listen(app.get('port'), function () {
     console.log('Angular cli listening on port ' + app.get('port'));
 });
