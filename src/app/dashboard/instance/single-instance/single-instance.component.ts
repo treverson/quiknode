5c92233cd4be6686be5c0c1108d6b53380da9db5
webosmotic-developer <dev.webosmotic@gmail.com>
2018-03-23 16:27:02 +0530

    Added small charts on instance cards.

--- src/app/dashboard/instance/single-instance/single-instance.component.ts
+++ src/app/dashboard/instance/single-instance/single-instance.component.ts
@@ -1,5 +1,8 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import * as HCs from 'highcharts';
+import {chart} from 'highcharts';
+import {InstanceService} from '../../../common/services/instance-service/instance.service';
 
 @Component({
     selector: 'app-single-instance',
@@ -15,15 +18,26 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     ]
 })
 export class SingleInstanceComponent implements OnInit {
+    @ViewChild('chartTarget1') chartTarget: ElementRef;
+    chart: HCs.ChartObject;
     @Input() instance: any;
     @Output() fnShowCreateModal =  new EventEmitter<any>();
     @Output() fnShowSuspendModal =  new EventEmitter<any>();
-    visibilityState: String = 'hidden';
+    visibilityState: String;
+    public getMetricObj: any;
 
-    constructor() {
+    constructor(private _instance: InstanceService) {
+        this.visibilityState = 'hidden';
+        this.getMetricObj = {
+            'instance-id': '',
+            'metrics-type': '',
+        };
     }
 
     ngOnInit() {
+        if (this.instance) {
+            this.fnGetMetric();
+        }
     }
 
     fadeInFadeOut() {
@@ -43,6 +57,88 @@ export class SingleInstanceComponent implements OnInit {
         e.preventDefault();
         this.fnShowCreateModal.next(this.instance);
 
+    }
+
+    fnGetMetric() {
+        this.getMetricObj['instance-id'] = this.instance['instance-id'];
+        this.getMetricObj['metrics-type'] = 'http-requests';
+        this._instance.fnGetMetric(this.getMetricObj).then((response: any) => {
+            if (response.values) {
+                setTimeout(() => {
+                    this.fnDisplayMetrics(response.columns, response.values);
+                }, 0);
+            }
+        });
+    }
+
+    fnDisplayMetrics(columns, values) {
+        const options: HCs.Options = {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: ''
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                type: 'datetime',
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                // lineColor: 'transparent',
+                labels: {
+                    enabled: false
+                },
+                minorTickLength: 0,
+                tickLength: 0
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                labels: {
+                    enabled: false
+                },
+                lineWidth: 0,
+                gridLineWidth: 0,
+                minorGridLineWidth: 0,
+            },
+            responsive: [{
+                rules: [{
+                    condition: {
+                        maxWidth: 200,
+                    },
+                    chartOptions: {
+                        showAxes: false
+                    }
+                }]
+            }],
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            tooltip: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    lineWidth: 2
+                }
+            },
+            series: [{
+                type: 'spline',
+                name: columns[1],
+                data: values,
+            }],
+        };
+
+        this.chart = chart(this.chartTarget.nativeElement, options);
     }
 
 }
