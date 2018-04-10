5fce5af3f9c07a7fdf3e9862ac6f9d196ff01c17
webosmotic-developer <dev.webosmotic@gmail.com>
2018-04-10 18:19:54 +0530

    Fixed minor issue. Increased idle time to 10 mins.

--- src/app/common/constant.ts
+++ src/app/common/constant.ts
@@ -1,6 +1,6 @@
 export class Constant {
     public static API_URL = 'api/v1/';
-    public static SESSION_IDLE_TIME = 120;
+    public static SESSION_IDLE_TIME = 500;
     public static SESSION_TIME = 30;
     public static IDLE_TIME_PING = 5;
 }
