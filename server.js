f13213bd61fc5341b38ebf5e2db1bc4e8d12c4cc
webosmotic-developer <dev.webosmotic@gmail.com>
2018-04-26 16:28:45 +0530

    Removed unneccesary code from server.js

--- server.js
+++ server.js
@@ -24,75 +24,9 @@ app.all("/api/*", function (req, res) {
         }
     })).pipe(res);
 });
-createTraffic();
 process.on('uncaughtException', function (err) {
     console.error('UNCAUGHT EXCEPTION: ' + err);
 });
 app.listen(app.get('port'), function () {
     console.log('Angular cli listening on port ' + app.get('port'));
 });
-
-function createTraffic() {
-    request.get('https://quiknode.pro/api/v1/account/instances', {
-        'auth': {
-            'username': 'wo.dev03@gmail.com',
-            'password': 'password',
-            'sendImmediately': false
-        }
-    }, function callback(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var reBody = JSON.parse(body).instances;
-            var headers = {"Content-Type": "application/json"};
-            var count = 0 ;
-            if (reBody) {
-                for (var i = 0; i < reBody.length; i++) {
-                    const obj = {
-                        'method': 'eth_blockNumber',
-                        'params': [],
-                        'id': 1,
-                        'jsonrpc': '2.0'
-                    };
-                    const configuration = reBody[i] && reBody[i].configuration;
-                    if (configuration && configuration.validators &&
-                        configuration.validators.http && configuration.validators.http.request) {
-                        if (configuration.validators.http.request) {
-                            // console.log('Validatore...', configuration.validators.http.request[0].type);
-                            const validateToken = _.find(configuration.validators.http.request, function(req) {
-                                return req.type === 'validateToken'
-                            });
-                            console.log('Validatore...10', validateToken);
-                            const validateReferer = _.find(configuration.validators.http.request, function(req){
-                                return req.type === 'validateReferer';
-                            });
-                            /* if (validateReferer) {
-                             console.log('validateReferer ', validateReferer.referers[0])
-                             headers  = new HttpHeaders().set('InterceptorSkipHeader', '').set('X-Alt-Referer', validateReferer.referers[0]);
-                             }*/
-                            console.log('Validatore...1', validateReferer);
-                            if (validateToken) {
-                                headers  = {'Authorization': validateToken.token,
-                                    "Content-Type": "application/json"};
-                            }
-                            if (validateReferer && validateReferer.referers) {
-                                headers  = {'X-Alt-Referer': validateReferer.referers[0],
-                                    "Content-Type": "application/json"};
-                            }
-                        }
-                    }
-                    request.post({ url: "https://" + reBody[i]['name'] + ".quiknode.pro/", json: obj, headers: headers, timeout: 10000 },
-                        function optionalCallback(err, httpResponse, body) {
-                            console.log("err ", err);
-                            count = count + 1;
-                            console.log('Count..',count, "/", reBody.length);
-                            if (count >= reBody.length) {
-                                createTraffic();
-                            }
-                        })
-                }
-
-            }
-        } else if (error) {
-            createTraffic();
-        }
-    });
-}
